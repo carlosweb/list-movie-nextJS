d1e80e3d1f4b79313dda8bfc9373449c6635cc35
carlos.santos <carlos.santos@bomconsorcio.com.br>
2021-03-25 23:22:44 -0300

    Fixed a bug from API

--- pages/api/movie/[id].js
+++ pages/api/movie/[id].js
@@ -1,5 +1,5 @@
 
-import { API_BASE, API_KEY } from '../../lib/tmdb'
+import { API_BASE, API_KEY } from '../../../lib/tmdb'
 
 export default async (req, res) => {
     const result = await fetch(`${API_BASE}/movie/${req.query.id}?api_key=${API_KEY}&language=en-US`)
--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -35,7 +35,7 @@ export default function MovieDetail({ info }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`http://localhost:3000/movie/${context.params.id}`)
+  const res = await fetch(`http://localhost:3000/api/movie/${context.params.id}`)
   const json = await res.json()
 
   return {
