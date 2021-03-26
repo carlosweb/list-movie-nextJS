88edfa1d8d1e9f445f03757bed9b37024ba20538
carlos.santos <carlos.santos@bomconsorcio.com.br>
2021-03-25 23:35:09 -0300

    Url Fixed v2.0

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -35,7 +35,7 @@ export default function MovieDetail({ info }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`https://find-movie-next-js.vercel.app/api/movie/${context.params.id}`)
+  const res = await fetch(`http://localhost:3000/api/movie/${context.params.id}`)
   const json = await res.json()
 
   return {
