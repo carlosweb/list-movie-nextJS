9679949b2c3d3bc8d08831f19c641044d0fc63ad
carlos.santos <carlos.santos@bomconsorcio.com.br>
2021-03-26 20:13:52 -0300

    URL Changed

--- pages/api/movie/[id].js
+++ pages/api/movie/[id].js
@@ -2,7 +2,7 @@
 import { API_BASE, API_KEY } from '../../../lib/tmdb'
 
 export default async (req, res) => {
-    const result = await fetch(`${API_BASE}/movie/${req.query.id}?api_key=${API_KEY}&language=en-US`)
+    const result = await fetch(`${process.env.API_BASE}/movie/${req.query.id}?api_key=${process.env.API_KEY}&language=en-US`)
     const json = await result.json()
 
     res.status(200).json({
--- pages/api/search.js
+++ pages/api/search.js
@@ -3,7 +3,7 @@ import { API_BASE, API_KEY } from '../../lib/tmdb'
 export default async (req, res) => {
     let q = req.query.q
 
-    const result = await fetch(`${API_BASE}/search/movie/?api_key=${API_KEY}&language=en-US&query=${q}`)
+    const result = await fetch(`${process.env.API_BASE}/search/movie/?api_key=${process.env.API_KEY}&language=en-US&query=${q}`)
     const json = await result.json()
 
     res.status(200).json({
