d0ee8eaf474f304f81d0e0b0b1cd1e41d1e9bc9f
carlos.santos <carlos.santos@bomconsorcio.com.br>
2021-03-26 12:27:28 -0300

    Url Fixed v3.0

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home({ list }) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch('https://find-movie-next-js.vercel.app/api/trending')
+  const res = await fetch('http://localhost:3000/api/trending')
   const json = await res.json()
 
   return {
